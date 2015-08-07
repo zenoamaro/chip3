1b8f447357d02c573c1da517a317041a631d3989
zenoamaro <zenoamaro@gmail.com>
2015-08-07 16:59:49 +0200

    Structured RAM contents viewer.

--- src/Workbench/RAMPane.jsx
+++ src/Workbench/RAMPane.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {PropTypes as T} from 'react';
 import Component from './Component';
 import Layout from './Layout';
-import {bitArray} from './utils';
+import {hexString, bitString, byteFormats} from './utils';
 
 
 export default class RAM extends Component {
@@ -14,8 +14,42 @@ export default class RAM extends Component {
 
 	static style = {
 		row: {
-			padding: '.7rem 1rem',
+			color: '#bbb',
 			borderBottom: 'solid thin #ddd',
+			fontFamily: 'lucida console, monospace',
+		},
+		currentRow: {
+			color: 'inherit',
+		},
+		cell: {
+			padding: '.93rem 1rem .7rem',
+			textAlign: 'center',
+		},
+		addr: {
+			flexBasis: 70,
+			textAlign: 'right',
+			borderRight: 'solid thin #ddd',
+		},
+		val: {
+			flexBasis: 70,
+			borderRight: 'solid thin #ddd',
+		},
+		dim: {
+			color: '#bbb',
+		},
+		bits: {
+			flexGrow: 1,
+			flexShrink: 0,
+		},
+		ar: {
+			color: '#af7bc5',
+			flexBasis: 70,
+			borderLeft: 'solid thin #ddd',
+		},
+		pc: {
+			color: '#e94c3d',
+			flexBasis: 70,
+			borderLeft: 'solid thin #ddd',
 		},
 	}
 
@@ -30,11 +64,41 @@ export default class RAM extends Component {
 	}
 
 	renderRow(word, i) {
+		const {style} = this;
+		const {ram, pc} = this.props;
+		const currentRow = i === this.props.ram.ar || i === this.props.pc;
+		const rowStyle = {...style.row, ...(currentRow && style.currentRow)};
 		return (
-			<div key={i} style={this.style.row}>
-				{i} - {word} - {bitArray(word).join('')}
-				{this.props.ram.ar === i && ' <AR'}
-				{this.props.pc === i && ' <PC'}
+			<Layout key={i} dir='horizontal'style={rowStyle}>
+				{this.renderCell({
+					style: style.addr,
+					hint: `Address - ${byteFormats(i)}`,
+					value: bitString(i, 5) })}
+				{this.renderCell({
+					style: style.val,
+					hint: `Address contents - ${byteFormats(i)}`,
+					value: hexString(word) })}
+				{this.renderCell({
+					style: style.bits,
+					value: bitString(word) })}
+				{this.renderCell({
+					style: style.ar,
+					hint: 'Address Register',
+					value: ram.ar === i && '<AR' })}
+				{this.renderCell({
+					style: style.pc,
+					hint: 'Program Counter',
+					value: pc === i && '<PC' })}
+			</Layout>
+		);
+	}
+
+	renderCell(cell) {
+		const {hint, style, value} = cell;
+		return (
+			<div title={hint}
+			     style={{...this.style.cell, ...style}}>
+				{value}
 			</div>
 		);
 	}
--- src/Workbench/utils.js
+++ src/Workbench/utils.js
@@ -18,13 +18,70 @@ export function clamp(min, x, max) {
 }
 
 /**
+ * Pads a string to a certain length by inserting filler characters on
+ * its left, until it matches the requested length.
+ *
+ * Spaces are filled the specified character. If the requested length is
+ * equal or less than the length of the string, the original string will
+ * be returned as is. Numbers will be cast to string.
+ *
+ * @param   {String|Number} x
+ * @param   {Number} len
+ * @param   {String} [fill='0']
+ * @returns {String}
+ */
+export function padString(x, len, fill='0') {
+	while (x.length < len) x = fill + x;
+	return x;
+}
+
+/**
+ * Returns a hex string representation of a number.
+ *
+ * @param   {Number} x
+ * @param   {Number} [pad=2]
+ * @returns {String}
+ */
+export function hexString(x, pad=2) {
+	const str = x.toString(16).toUpperCase();
+	return `0x${padString(str, pad)}`;
+}
+
+/**
  * Converts a byte to an array of bits.
  *
  * @method  bitArray
  * @param   {Number} x
+ * @param   {Number} [len=8]
  * @returns {Array}
  */
-export function bitArray(x) {
-	const digits = [ 128, 64, 32, 16, 8, 4, 2, 1 ];
+export function bitArray(x, len=8) {
+	const digits = [ 128, 64, 32, 16, 8, 4, 2, 1 ].slice(len * -1);
 	return digits.map(d => (x & d) > 0? 1 : 0);
+}
+
+/**
+ * Converts a byte to string of bits.
+ *
+ * @param   {Number} x
+ * @param   {Number} [len]
+ * @returns {String}
+ */
+export function bitString(x, len) {
+	return bitArray(x, len).join('');
+}
+
+/**
+ * Returns a string with the value displayed in decimal, hexadecimal and
+ * binary formats.
+ *
+ * @param   {Number} x
+ * @returns {String}
+ */
+export function byteFormats(x) {
+	return [
+		`DEC=${x}`,
+		`HEX=${hexString(x)}`,
+		`BIN=${bitString(x)}`,
+	].join(' ');
 }
