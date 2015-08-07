1de1c65bdd9f1c1af17f9965d0ef7751f33a39f6
zenoamaro <zenoamaro@gmail.com>
2015-08-07 19:00:10 +0200

    Highlighting memory activity.

--- src/Workbench/RAMPane.jsx
+++ src/Workbench/RAMPane.jsx
@@ -21,6 +21,12 @@ export default class RAM extends Component {
 		currentRow: {
 			color: 'inherit',
 		},
+		arRow: {
+			background: '#e6f5ff',
+		},
+		pcRow: {
+			background: '#ffefed',
+		},
 		cell: {
 			padding: '.93rem 1rem .7rem',
 			textAlign: 'center',
@@ -28,11 +34,11 @@ export default class RAM extends Component {
 		addr: {
 			flexBasis: 70,
 			textAlign: 'right',
-			borderRight: 'solid thin #ddd',
+			borderRight: 'solid thin rgba(0, 0, 0, .14)',
 		},
 		val: {
 			flexBasis: 70,
-			borderRight: 'solid thin #ddd',
+			borderRight: 'solid thin rgba(0, 0, 0, .14)',
 		},
 		dim: {
 			color: '#bbb',
@@ -41,15 +47,21 @@ export default class RAM extends Component {
 			flexGrow: 1,
 			flexShrink: 0,
 		},
+		read: {
+			color: '#0099ff',
+		},
+		write: {
+			color: '#e94c3d',
+		},
 		ar: {
-			color: '#af7bc5',
+			color: '#0099ff',
 			flexBasis: 70,
-			borderLeft: 'solid thin #ddd',
+			borderLeft: 'solid thin rgba(0, 0, 0, .14)',
 		},
 		pc: {
 			color: '#e94c3d',
 			flexBasis: 70,
-			borderLeft: 'solid thin #ddd',
+			borderLeft: 'solid thin rgba(0, 0, 0, .14)',
 		},
 	}
 
@@ -66,8 +78,19 @@ export default class RAM extends Component {
 	renderRow(word, i) {
 		const {style} = this;
 		const {ram, pc} = this.props;
-		const currentRow = i === this.props.ram.ar || i === this.props.pc;
-		const rowStyle = {...style.row, ...(currentRow && style.currentRow)};
+		const activity = i===ram.ar
+			          && ((ram.read && 'READ') || (ram.write && 'WRITE'));
+		const rowStyle = {
+			...style.row,
+			...(i===ram.ar && style.arRow),
+			...(i===pc && style.pcRow),
+			...((i===ram.ar || i===pc) && style.currentRow),
+		};
+		const activityStyle = {
+			...style.ar,
+			...(ram.read && style.read),
+			...(ram.write && style.write),
+		};
 		return (
 			<Layout key={i} dir='horizontal'style={rowStyle}>
 				{this.renderCell({
@@ -81,6 +104,10 @@ export default class RAM extends Component {
 				{this.renderCell({
 					style: style.bits,
 					value: bitString(word) })}
+				{this.renderCell({
+					style: activityStyle,
+					hint: 'Read/write activity',
+					value: activity })}
 				{this.renderCell({
 					style: style.ar,
 					hint: 'Address Register',
